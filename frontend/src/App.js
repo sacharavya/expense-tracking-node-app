8a30dfe4e00c43fdf52629c48ed17ba89adfad68
sacharavya <sacharavya@gmail.com>
2021-12-04 10:54:14 +0545

    login section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 /*
  *Import react packages here
  */
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 /*
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,26 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useLocation } from 'react-router-dom'
 import { Form, Col, Row, Button } from 'react-bootstrap'
-// import Message from '../components/Message'
-// import Loader from '../components/Loader'
+
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 
-const LoginScreen = () => {
-  // const redirect = location.search ? location.search.split('=')[1] : '/'
+import { login } from '../actions/userActions'
 
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search)
+}
+
+const LoginScreen = ({ history }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  // Process the query search string and destructure a redirect search
+  // value, providing a "/" fallback if there is no redirect search value
+  const { redirect = '/dashboard' } = useQuery()
+
+  useEffect(() => {
+    if (userInfo) {
+      // history.push(redirect)
+      console.log('Nice')
+      console.log(userInfo)
+    }
+  }, [history, userInfo])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(login(email, password))
+  }
   return (
     <FormContainer>
       <h1>Sign In</h1>
-      <Form>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
         <Form.Group controlId='email'>
           <Form.Label>Email Adress</Form.Label>
-          <Form.Control type='email' placeholder='Enter email'></Form.Control>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group controlId='password'>
           <Form.Label>password</Form.Label>
           <Form.Control
             type='password'
             placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
@@ -30,7 +69,11 @@ const LoginScreen = () => {
       </Form>
       <Row className='py-3'>
         <Col>
-          New Customer? <Link to='/register'> Register </Link>{' '}
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            {' '}
+            Register
+          </Link>
         </Col>
       </Row>
       <Row className='py-2'>
